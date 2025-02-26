a0c21acf41217bd644d00b15428d9b7c2a6b8839
FLX <cauerast@gmail.com>
2025-02-26 11:26:45 -0300

    version1

--- code/script.js
+++ code/script.js
@@ -105,9 +105,70 @@ console.log(min);
 */
 
 // RANDOM NUMBER GENERATOR //
+/* 
+
 const min = 50;
 const max = 100;
 
 
 let randomNum = Math.floor(Math.random() * (max - min)) + min;
 console.log(randomNum)
+
+*/
+
+/*
+// make a dice // 
+const myButton = document.getElementById(`myButton`);
+const label1 = document.getElementById(`label1`);
+const label2 = document.getElementById(`label2`);
+const label3 = document.getElementById(`label3`);
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
+*/
+
+
+
+
+
+const myText = document.getElementById(`myText`)
+const mySubmit = document.getElementById(`mySubmit`)
+const resultElement = document.getElementById(`resultElement`)
+let age
+
+mySubmit.onclick = function(){
+
+    age = myText.value;
+    age = Number(age);
+
+    if(age >= 100){
+        resultElement.textContent = `You are too OLD to enter this site`
+    }
+    
+    else if(age == 0){
+        resultElement.textContent = `You were just born!`
+    }
+    
+    else if(age >= 18){
+        resultElement.textContent = `Your are old enough to enter this site`
+    }
+    
+    else if(age < 0){
+        resultElement.textContent = `Your age can't be below 0`
+    }
+    
+    else{
+        resultElement.textContent = `You must be 18+ to enter this site`
+    }
+}
