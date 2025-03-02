84249191e4e00032b448ea3da81b68ee254b25a1
FLX <cauerast@gmail.com>
2025-03-01 22:37:49 -0300

    version1

--- code/script.js
+++ code/script.js
@@ -459,7 +459,103 @@ while(!loggedIn){
 
 
 // FOR LOOP //
-for(let i = 0; i <= 2; i++){
-    console.log(i)
+/*
+for(let i = 1; i <= 20; i++){
+    if(i == 13){
+        break;
+    }
+    else{
+        console.log(i);
+    }
 }
+*/
+
+
+// NUMBER GUESSING GAME //
+/*
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
+console.log(answer);
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+    
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`)
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert(`Please enter a valid number!`);
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert(`Please enter a valid number!`)
+    }
+    else{
+        attempts++;
+        if(guess < answer){
+            window.alert(`TOO LOW! TRY AGAIN!`);
+        }
+        else if(guess > answer){
+            window.alert(`TOO HIGH! TRY AGAIN!`)
+        }
+        else{
+            window.alert(`CORRECT! The answer is ${answer}. It tooks to you ${attempts} attempts!`)
+            running = false;
+        }
+    }
+}
+*/
+
+
+// FUNCTIONS // -- Love this part --
+/*
+function happyBirthday(username, age){
+    console.log(`Happy Birthday to you`);
+    console.log(`Happy Birthday to you`);
+    console.log(`Happy Birthday dear ${username}`);
+    console.log(`Happy Birthday to you`);
+    console.log(`You are ${age} years old`);
+}
+
+happyBirthday("Flx", 19);
+*/
+
+/*
+function add(x ,y){
+    return x + y;
+}
+function subtract(x, y){
+    return x - y;
+}
+function multiply(x, y){
+    return x * y;
+}
+function divide(x, y){
+    return x / y;
+}
+function isEven(number){
+    if(number % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+    // u can use this too --- (  return number % 2 === 0 ? true : false;  )
+}
+function isValidEmail(email){
+    if(email.includes(`@`)){
+        return true;
+    }
+    else{
+        return false
+    }
+
+    // u can do this w/ --- (  return email.includes(`@`) ?  true : false;  )
+}
+console.log(isValidEmail(`bro@fake.com`));
+*/
 
