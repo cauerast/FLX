be62c8b4b992e3f100e5f4fae4aefa26270b91a0
FLX <cauerast@gmail.com>
2025-03-03 00:41:53 -0300

    learn how to create a temperature conversion on js lol

--- code/script.js
+++ code/script.js
@@ -581,4 +581,51 @@ function function2(){
 
 
 // Temperature conversion js //
+/*
+const textBox = document.getElementById(`textBox`);
+const toFahrenheit = document.getElementById(`toFahrenheit`);
+const toCelsius = document.getElementById(`toCelsius`);
+const result = document.getElementById(`result`);
+let temp;
 
+function convert(){
+    if(toFahrenheit.checked){
+        temp = Number(textBox.value);
+        temp = (temp * 9 / 5 + 32);
+        result.textContent = temp.toFixed(1) + `°F`;
+
+    }
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = ((temp - 32) * (5 / 9));
+        result.textContent = temp.toFixed(1) + `°C`;
+
+    }
+    else{
+        result.textContent = `Select a unit`
+    }
+}
+    */
+
+const textBox = document.getElementById(`textBox`);
+const toFahrenheit = document.getElementById(`toFahrenheit`);
+const toCelsius = document.getElementById(`toCelsius`);
+const submitBtn = document.getElementById(`submitBtn`);
+const result = document.getElementById(`result`);
+let temp;
+
+submitBtn.onclick = function convert(){
+    if(toFahrenheit.checked){
+        temp = Number(textBox.value);
+        temp = (temp * 9 / 5 + 32);
+        result.textContent = temp.toFixed(2) + `°F`;
+    }
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5 / 9);
+        result.textContent = temp.toFixed(2) + `°C`;
+    }
+    else{
+        result.textContent = `Select a unit to convert`
+    }
+}
