6f12ec95217276b627050d8649477f17bea439eb
FLX <cauerast@gmail.com>
2025-03-04 13:10:07 -0300

    version1

--- code/script.js
+++ code/script.js
@@ -792,3 +792,36 @@ function sum(...numbers){
 const total = sum(1, 2, 3, 4, 5);
 console.log(`your total is $${total}`);
 */
+
+/*
+function sum(...numbers){
+    let result = 0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result;
+}
+const total = sum(1, 2, 3, 4, 5);
+console.log(`your total is $${total}`);
+*/
+
+/*
+function getAverage(...numbers){
+    let result = 0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result / numbers.length;
+}
+const total = getAverage(75, 100, 85, 90, 50);
+console.log(total);
+*/
+
+/*
+function combineStrings(...strings){
+    return strings.join(` `);
+}
+const fullName = combineStrings(`Mr.`, `Spongebob`, `Squarepants`, `III`);
+console.log(fullName);
+*/
+
