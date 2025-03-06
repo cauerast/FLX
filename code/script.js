e64a1b3dceb0fa8fbd293faadff722940961e47a
FLX <cauerast@gmail.com>
2025-03-06 19:18:46 -0300

    version1

--- code/script.js
+++ code/script.js
@@ -939,6 +939,7 @@ function display(element){
 */
 
  // -- fruits -- // 
+ /*
 let fruits = [`apple`, `orange`, `banana`, `coconut`];
 
 fruits.forEach(capitalize);
@@ -961,4 +962,53 @@ function capitalize(element, index, array ){
 function display(element){
     console.log(element);
 }
+*/
 
+
+
+// .map() - accepts callback and applies that function to each element of an array, then return a new array // 
+/*
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(square);
+const cubes = numbers.map(cube);
+
+console.log(cubes);
+
+function square(element){
+    return Math.pow(element, 2);
+}
+
+function cube(element){
+    return Math.pow(element, 3);
+} 
+
+*/
+
+
+/*
+const students = [`spongebob`, `patrick`, `squidward`, `sandy`];
+const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(lowerCase);
+
+
+console.log(studentsLower);
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+
+function lowerCase(element){
+    return element.toLowerCase();
+}
+    */
+
+
+
+const dates = [`2024-1-10`, `2025-2-20`, `2026-3-20`];
+const formattedDates = dates.map(formatDates);
+
+
+function formatDates(element){
+    const parts = element.split(`-`);
+    return `${parts[1]}/${parts[2]}/${parts[0]}`;
+}
