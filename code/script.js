47b02f1e8414912559af1b617d5b194f4906fd78
FLX <cauerast@gmail.com>
2025-03-10 17:30:49 -0300

    classes

--- code/script.js
+++ code/script.js
@@ -1288,3 +1288,32 @@ car4.drive();
 
 
 // class = provides a more structured and cleaner way to work with objects compared to tradicional contructor functions - ex. static keyword, encapsulation, inheritance.
+/*
+
+class product{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+
+    displayProduct(){
+        console.log(`Product: ${this.name}`);
+        console.log(`Price: ${this.price.toFixed(2)}`);
+    }
+
+    calculateTotal(salesTax){
+        return this.price + (this.price * salesTax);
+    }
+}
+
+const salesTax = 0.05;
+const product1 = new product(`Shirt`, 19.99);
+const product2 = new product(`Pants`, 22.50);
+const product3 = new product(`underwear`, 100.00);
+
+let productShowed = product2.displayProduct();
+
+
+const total = productShowed.calculateTotal(salesTax);
+console.log(`Your total is ${total.toFixed(2)}`);
+*/
