95917807775dc37a03a6ad7888bdc221ad126d41
FLX <cauerast@gmail.com>
2025-03-29 21:58:06 -0300

    nothing to see here

--- code/script.js
+++ code/script.js
@@ -1971,8 +1971,9 @@ console.log(`The final score is ${game.getScore()}pts`)
 // setTimeout() = function in JS that allows you to schedule the execution of a function after an amount of time (ms). Times are approximate (varies based on the workload of the JS runtime env.)
 // -- setTimeout(callback, delay)
 
-
+/*
 function sayHello(){
     window.alert("!")
 }
 setTimeout(sayHello, 3000);
+*/
